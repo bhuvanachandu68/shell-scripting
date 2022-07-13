b9f04280c594bef4236f1c7648d2bbc729f0c840
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-13 17:35:26 +0530

    This is my first commit

--- colour.sh
+++ colour.sh
@@ -1,3 +1,7 @@
 #bin/bash!
 ## colour
 echo -e "\e[33m IM priniting some colour \e[0m"
+echo -e "\e[34m IM priniting some colour \e[0m"
+echo -e "\e[35m IM priniting some colour \e[0m"
+echo -e "\e[31m IM priniting some colour \e[0m"
+echo -e "\e[37m IM priniting some colour \e[0m"
