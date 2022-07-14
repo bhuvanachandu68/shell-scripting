23fc7751aaa82362117d04ba4d547b945ed48559
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-14 12:48:48 +0530

    This is my first commit

--- 05special-var.sh
+++ 05special-var.sh
@@ -4,10 +4,13 @@
 #$1, $2,---$9
 #$* used to show the variables
 #,$@,$$
+#$ is to print the number of variables
+
 a=qoo
 b=poias
 echo "this is $0"
 echo"declared values are $a $b"
 echo "the value of x is $1"
 echo "The name of trainer is  $2"
-echo "$*"
+echo "cli variables $*"
+echo "the number of variables are $#"
