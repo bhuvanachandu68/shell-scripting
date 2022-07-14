8ad643ad91c4e10ec120b47a73d9fc0d60521623
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-14 12:42:17 +0530

    This is my first commit

--- 05special-var.sh
+++ 05special-var.sh
@@ -2,7 +2,10 @@
 #$0 : print the script name that we are executing
 #$?
 #$1, $2,---$9
-#&*,$@,$$
-echo "the value of x is $1"
-echo "The name of trainer is  $2"
+#$* used to show the variables
+#,$@,$$
+a=qoo
+b=poias
+echo "the value of x is $a"
+echo "The name of trainer is  $b"
 echo "$*"
