92f9dc7976747217b57a3ba4fda1d898b192ed3c
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-14 14:26:56 +0530

    This is my first commit

--- 06function.sh
+++ 06function.sh
@@ -1,10 +1,15 @@
 #!bin/bash
 ## function helps to write the repetative code
-
+sam(){
+    echo "hello I'm Sam"
+}
 f(){
     echo "the number of useres loged in are $(who|wc -l)"
+    sam
 }
 echo "hello:)"
+f
+sleep 5
 f
 sleep 5
 f
