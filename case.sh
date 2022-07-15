baa6b941335d25afab576d63be670949cf8d1b0a
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-15 16:33:37 +0530

    This is my first commit

--- case.sh
+++ case.sh
@@ -3,13 +3,16 @@
 action=$1
 case $action in 
 start) 
-echo "hello";;
-exit 0
+echo "hello"
+exit 0;;
+
 stop)
-echo   "commands";;
-exit 0
+echo   "commands"
+exit 0;;
+
 *)
 echo "bye"
 exit 1
 ;;
+$?
 esac
