2600aa500135b46ba6246cbfdafdd73b34ffaf64
bhuvanachandu68 <bhuvanachandu68@gmail.com>
2022-07-15 17:24:27 +0530

    This is my first commit

--- if.sh
+++ if.sh
@@ -4,6 +4,12 @@ ACTION=$1
 if [ "$ACTION" = "start" ];
 then 
 echo -e "selected option is \e[32m start \e[0m"
+elif[ "$ACTION" = "stop" ];
+then 
+echo -e "selected option is \e[33m stop \e[0m"
+elif[ "$ACTION" = "restart" ];
+then 
+echo -e "selected option is \e[35m restart \e[0m"
 else 
 echo -e "selected option is \e[34m select valid option \e[0m"
 fi
